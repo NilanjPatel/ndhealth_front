016cff490ad8c9330c4766c2e2ec548611e55606
Nilanj <nilanjpatel@yahoo.in>
2025-08-07 01:26:53 -0400

    terminal update v1

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,9 +1,9 @@
 // const API_BASE_URL = 'http://172.20.10.12:8000'; // Replace with your Django server URL
 // const API_BASE_URL = 'https://7643e476ea3c.ngrok-free.app'; // Replace with your Django server URL
 // const API_BASE_URL = 'http://192.168.1.55:9000'; // Replace with your Django server URL
-// const API_BASE_URL = "https://api.nd-health.ca";
+const API_BASE_URL = "https://api.nd-health.ca";
 // const API_BASE_URL = "http://192.168.88.151:9000"; // Replace with your Django server URL
-const API_BASE_URL = "http://10.0.0.130:9000"; // Replace with your Django server URL
+// const API_BASE_URL = "http://10.0.0.130:9000"; // Replace with your Django server URL
 const API_BASE_PATH = `${API_BASE_URL}/api`;
 
 export default API_BASE_PATH;
